425d57be6c629763f489a9fea5b503dd6a3b4067
Md Arafat <57371182+arafat1802@users.noreply.github.com>
2025-04-05 12:03:22 +0600

    password and jwt handler added

--- backend/core/config.py
+++ backend/core/config.py
@@ -5,5 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 class Settings: 
     DATABASE_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 settings = Settings()
